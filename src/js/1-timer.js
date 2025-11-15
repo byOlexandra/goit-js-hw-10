6edd186074881df0bd66c982829bee61dce8a750
devolexa <starovatovasasha@gmail.com>
2025-11-15 16:33:53 +0200

    task one

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,11 +1,16 @@
-// Описаний в документації
 import flatpickr from "flatpickr";
-// Додатковий імпорт стилів
 import "flatpickr/dist/flatpickr.min.css";
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
 
 const refs = {
     input: document.querySelector("#datetime-picker"),
-    startBtn: document.querySelector("[data-start]")
+    startBtn: document.querySelector("[data-start]"),
+    days: document.querySelector("[data-days]"),
+    hours: document.querySelector("[data-hours]"),
+    minutes: document.querySelector("[data-minutes]"),
+    seconds: document.querySelector("[data-seconds]")
 }
 
 let userSelectedDate = null;
@@ -20,7 +25,13 @@ const options = {
     onClose(selectedDates) {
         const selectedDate = selectedDates[0];
         if (selectedDate <= new Date()) {
-        window.alert("Please choose a date in the future");
+        iziToast.error({
+            title: 'Error',
+            message: 'Please choose a date in the future',
+            position: "topRight",
+            backgroundColor: '#EF4040',
+            iconUrl: '../img/bi_x-octagon.svg'
+        });
         refs.startBtn.disabled = true;
         return;
         }
@@ -29,4 +40,45 @@ const options = {
     },
 };
 
-flatpickr(refs.input, options)
+flatpickr(refs.input, options);
+
+refs.startBtn.addEventListener("click", e => {
+    e.preventDefault();
+    refs.input.disabled = true;
+    refs.startBtn.disabled = true;
+
+    timerId = setInterval(() => {
+        const currentTime = Date.now();
+        const timeLeftMs = userSelectedDate - currentTime;
+        if (timeLeftMs <= 0) {
+            clearInterval(timerId);
+            refs.input.disabled = false;
+            return;
+        }
+        const time = convertMs(timeLeftMs);
+        updateClockFace(time)
+    }, 1000)
+})
+
+function convertMs(ms) {
+        const second = 1000;
+        const minute = second * 60;
+        const hour = minute * 60;
+        const day = hour * 24;
+        const days = Math.floor(ms / day);
+        const hours = Math.floor((ms % day) / hour);
+        const minutes = Math.floor(((ms % day) % hour) / minute);
+        const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+        return { days, hours, minutes, seconds };
+}
+function updateClockFace({ days, hours, minutes, seconds }) {
+    refs.days.textContent = days;
+    refs.hours.textContent = addLeadingZero(hours);
+    refs.minutes.textContent = addLeadingZero(minutes);
+    refs.seconds.textContent = addLeadingZero(seconds);
+}
+
+function addLeadingZero(value) {
+    return String(value).padStart(2, "0");
+}
